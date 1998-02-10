af6f859b0707ad0252882309b973f03db25e93a7
Arturo Espinosa <unammx@src.gnome.org>
1998-02-10 21:28:35 +0000

    New configure framework is in place

--- libgnomeui/libgnomeui.h
+++ libgnomeui/libgnomeui.h
@@ -2,14 +2,16 @@
 #define LIBGNOMEUI_H
 
 #include "libgnome/gnome-defs.h"
+#include "libgnomeui/gnome-about.h"
 #include "libgnomeui/gnome-app.h"
 #include "libgnomeui/gnome-app-helper.h"
 #include "libgnomeui/gnome-actionarea.h"
 #include "libgnomeui/gnome-colors.h"
 #include "libgnomeui/gnome-color-selector.h"
+#include "libgnomeui/gnome-dns.h"
 #include "libgnomeui/gnome-font-selector.h"
 #include "libgnomeui/gnome-messagebox.h"
-#include "libgnomeui/gnome-about.h"
+#include "libgnomeui/gnome-net.h"
 #include "libgnomeui/gnome-pixmap.h"
 #include "libgnomeui/gnome-actionarea.h"
 #include "libgnomeui/gnome-scores.h"
