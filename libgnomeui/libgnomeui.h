f43b6cc7b0dcddf5f8acca4237bcbd933aefe3bb
Federico Mena Quintero <federico@nuclecu.unam.mx>
1998-03-19 01:19:01 +0000

    New files gnome-file-entry.[ch]. This is a combo box widget with an extra
    1998-03-18  Federico Mena Quintero  <federico@nuclecu.unam.mx>
    
    	* gnome-file-entry.h: New files gnome-file-entry.[ch].  This is a
    	combo box widget with an extra "Browse" button to enter file
    	names.  All places in Gnome where an entry widget is being used to
    	get a filename should use the GnomeFileEntry widget instead.
    
    	* libgnomeui.h: Added gnome-file-entry.h to the includes.
    
    	* Makefile.am: Added gnome-file-entry.[ch] to the file lists.
    
    1998-03-18  Federico Mena Quintero  <federico@nuclecu.unam.mx>
    
    	* POTFILES.in: Added libgnomeui/gnome-file-entry.c to the list.

--- libgnomeui/libgnomeui.h
+++ libgnomeui/libgnomeui.h
@@ -10,6 +10,7 @@
 #include "libgnomeui/gnome-color-selector.h"
 #include "libgnomeui/gnome-dns.h"
 #include "libgnomeui/gnome-entry.h"
+#include "libgnomeui/gnome-file-entry.h"
 #include "libgnomeui/gnome-font-selector.h"
 #include "libgnomeui/gnome-messagebox.h"
 #include "libgnomeui/gnome-net.h"
