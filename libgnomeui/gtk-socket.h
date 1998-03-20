c4a5d889f9a270be2fc2bfeb8399bda53c545c56
Tom Tromey <tromey@cygnus.com>
1998-03-20 07:13:19 +0000

    Changed `()' to `(void)'. Renamed `clock' arguments to `gclock' to avoid
    Fri Mar 20 00:01:47 1998  Tom Tromey  <tromey@cygnus.com>
    
    	* gtk-plug.h: Changed `()' to `(void)'.
    	* gtk-clock.h: Renamed `clock' arguments to `gclock' to avoid
    	clash with ANSI function.
    	* gtk-socket.h: Renamed `socket' argument to `gsocket' to avoid
    	clash with Unix system call.

--- libgnomeui/gtk-socket.h
+++ libgnomeui/gtk-socket.h
@@ -1,5 +1,5 @@
 /* GTK - The GIMP Toolkit
- * Copyright (C) 1995-1997 Peter Mattis, Spencer Kimball and Josh MacDonald
+ * Copyright (C) 1995-1998 Peter Mattis, Spencer Kimball and Josh MacDonald
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -56,7 +56,7 @@ struct _GtkSocketClass
 
 GtkWidget*     gtk_socket_new      ();
 guint          gtk_socket_get_type (void );
-void           gtk_socket_steal    (GtkSocket *socket,
+void           gtk_socket_steal    (GtkSocket *gsocket,
 				    guint32 wid);
 
 #ifdef __cplusplus
