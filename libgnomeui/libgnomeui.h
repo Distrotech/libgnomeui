2232ee51180ab7b48b0c2ec7fa9b89959e3693cb
Arturo Espinosa <unammx@src.gnome.org>
1998-04-29 05:28:54 +0000

    Add geometry parsing routine to gnome library -mig

--- libgnomeui/libgnomeui.h
+++ libgnomeui/libgnomeui.h
@@ -14,6 +14,7 @@
 #include "libgnomeui/gnome-entry.h"
 #include "libgnomeui/gnome-file-entry.h"
 #include "libgnomeui/gnome-font-selector.h"
+#include "libgnomeui/gnome-geometry.h"
 #include "libgnomeui/gnome-icon-list.h"
 #include "libgnomeui/gnome-messagebox.h"
 #include "libgnomeui/gnome-net.h"
