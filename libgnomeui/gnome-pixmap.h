ff0f7105044a757d35a64bf85047d5d0b325a656
Federico Mena Quintero <federico@nuclecu.unam.mx>
1998-06-11 02:49:53 +0000

    #include <config.h>
    1998-06-10  Federico Mena Quintero  <federico@nuclecu.unam.mx>
    
    	* gnome-calculator.c:
    	* gnome-properties.c: #include <config.h>
    
    	* gnome-pixmap.[ch]: Removed the *old* gnome_pixmap_* functions
    	that predated the GnomePixmap widget.

--- libgnomeui/gnome-pixmap.h
+++ libgnomeui/gnome-pixmap.h
@@ -60,44 +60,6 @@ void       gnome_pixmap_load_rgb_d_at_size     (GnomePixmap *gpixmap, unsigned c
 						int width, int height);
 
 
-
-/* FIXME:  These are the old gnome-pixmap API functions.  They should
- * be flushed out in favor of the GnomePixmap widget.
- */
-
-void gnome_destroy_pixmap_cache (void);
-
-void gnome_create_pixmap_gdk              (GdkWindow *window,
-					   GdkPixmap **pixmap,
-					   GdkBitmap **mask,
-					   GdkColor  *transparent,
-					   char *file);
-void gnome_create_pixmap_gtk              (GtkWidget *window,
-					   GdkPixmap **pixmap,
-					   GdkBitmap **mask,
-					   GtkWidget *holder,
-					   char *file);
-void gnome_create_pixmap_gtk_d            (GtkWidget *window,
-					   GdkPixmap **pixmap,
-					   GdkBitmap **mask,
-					   GtkWidget *holder,
-					   char **data);
-GtkWidget *gnome_create_pixmap_widget     (GtkWidget *window,
-					   GtkWidget *holder,
-					   char *file);
-GtkWidget *gnome_create_pixmap_widget_d   (GtkWidget *window,
-					   GtkWidget *holder,
-					   char **data);
-void gnome_set_pixmap_widget              (GtkPixmap *pixmap,	
-					   GtkWidget *window,
-					   GtkWidget *holder,
-					   char *file);
-void gnome_set_pixmap_widget_d            (GtkPixmap *pixmap,	
-					   GtkWidget *window,
-					   GtkWidget *holder,
-					   char **data);
-
-
 END_GNOME_DECLS
 
 
