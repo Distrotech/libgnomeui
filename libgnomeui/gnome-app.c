2f647caa992390bf8f7441509427d405277e6a31
Miguel de Icaza <miguel@nuclecu.unam.mx>
1998-06-28 21:21:05 +0000

    i18n strings marked; Added code that still does not work for configuring
    1998-06-28  Miguel de Icaza  <miguel@nuclecu.unam.mx>
    
    	* gnome-app.c (make_button_toolbar): i18n strings marked;  Added
    	code that still does not work for configuring toolbar settings (we
    	should configure pictures/text-pictures/text-only/tooltips).

--- libgnomeui/gnome-app.c
+++ libgnomeui/gnome-app.c
@@ -8,7 +8,9 @@
  * and I don't know what you other people did :)
  */
 #include "config.h"
+#include <glib.h>
 #include "libgnome/gnome-defs.h"
+#include "libgnome/gnome-i18n.h"
 #include "libgnome/gnome-util.h"
 #include "libgnome/gnome-config.h"
 #include "gnome-app.h"
@@ -338,8 +340,8 @@ make_button_menubar(GnomeApp *app)
 	int i;
 	
 	menu_menu = gtk_menu_new();
-	menuitems[0] = gtk_menu_item_new_with_label("Top");
-	menuitems[1] = gtk_menu_item_new_with_label("Bottom");
+	menuitems [0] = gtk_menu_item_new_with_label(_("Top"));
+	menuitems [1] = gtk_menu_item_new_with_label(_("Bottom"));
 
 	for(i = 0; i < 2; i++)
 	{
@@ -351,41 +353,57 @@ make_button_menubar(GnomeApp *app)
 }
 
 static void
+configure_toolbar (GtkMenuItem *menu_item, GnomeApp *app)
+{
+	/* FIXME: add toolbar configuration dialog box here */
+}
+			       
+static void
 make_button_toolbar (GnomeApp *app)
 {
 	int i;
 
-	toolbar_menu = gtk_menu_new();
+	toolbar_menu = gtk_menu_new ();
 
-	toolitems[0] = gtk_menu_item_new_with_label("Top");
-	toolitems[1] = gtk_menu_item_new_with_label("Bottom");
-	toolitems[2] = gtk_menu_item_new_with_label("Left");
-	toolitems[3] = gtk_menu_item_new_with_label("Right");
+	toolitems [0] = gtk_menu_item_new_with_label(_("Top"));
+	toolitems [1] = gtk_menu_item_new_with_label(_("Bottom"));
+	toolitems [2] = gtk_menu_item_new_with_label(_("Left"));
+	toolitems [3] = gtk_menu_item_new_with_label(_("Right"));
+	toolitems [4] = gtk_menu_item_new_with_label(_("Configure toolbar"));
 
-	for(i = 0; i < 4; i++)
+	/* Change 4 to 5 to do the toolbar configuration code */
+	for (i = 0; i < 4; i++)
 	{
-		gtk_widget_show(toolitems[i]);
-		gtk_menu_append(GTK_MENU(toolbar_menu), toolitems[i]);
-		gtk_signal_connect(GTK_OBJECT(toolitems[i]), "activate",
-				   GTK_SIGNAL_FUNC(gnome_app_setpos_activate_toolbar), app);
+		GtkSignalFunc function;
+		
+		gtk_widget_show (toolitems [i]);
+		gtk_menu_append (GTK_MENU (toolbar_menu), toolitems [i]);
+		
+		if (i == 4)
+			function = GTK_SIGNAL_FUNC (configure_toolbar);
+		else
+			function = GTK_SIGNAL_FUNC (gnome_app_setpos_activate_toolbar);
+		
+		gtk_signal_connect (GTK_OBJECT(toolitems[i]), "activate", function, app);
 	}
 }
 
 static void
 gnome_app_rightclick_menubar(GtkWidget *widget, GdkEventButton *event, GnomeApp *app)
 {
-	g_return_if_fail(app != NULL);
-	g_return_if_fail(GNOME_IS_APP(app));
+	g_return_if_fail (app != NULL);
+	g_return_if_fail (GNOME_IS_APP (app));
 
-	if(menu_menu == NULL) make_button_menubar (app);
+	if (menu_menu == NULL)
+		make_button_menubar (app);
 
-	if(GTK_WIDGET_VISIBLE(menu_menu))
-		gtk_menu_popdown(GTK_MENU(menu_menu));
+	if (GTK_WIDGET_VISIBLE (menu_menu))
+		gtk_menu_popdown (GTK_MENU (menu_menu));
 
 	if(event->button != 3)
 		return;
 
-	gtk_menu_popup(GTK_MENU(menu_menu),
+	gtk_menu_popup (GTK_MENU (menu_menu),
 		       NULL, NULL, NULL, NULL,
 		       event->button,
 		       event->time);
@@ -397,12 +415,20 @@ gnome_app_rightclick_toolbar(GtkWidget *widget, GdkEventButton *event, GnomeApp 
 	g_return_if_fail(app != NULL);
 	g_return_if_fail(GNOME_IS_APP(app));
 
-	if(toolbar_menu == NULL) make_button_toolbar (app);
+	/* Double click on handlebox, reparents */
+	if (event->button == 1 && event->type == GDK_2BUTTON_PRESS){
+		/* FIXME: Mhm, there is no easy way to tell the handlebox to reparent itself */
+		return;
+	}
+		
+	/* Right click on handlebox, brings up menu */
+	if (toolbar_menu == NULL)
+		make_button_toolbar (app);
 
-	if(GTK_WIDGET_VISIBLE(toolbar_menu))
+	if (GTK_WIDGET_VISIBLE(toolbar_menu))
 		gtk_menu_popdown(GTK_MENU(toolbar_menu));
 
-	if(event->button != 3)
+	if (event->button != 3)
 		return;
 
 	gtk_menu_popup(GTK_MENU(toolbar_menu),
@@ -523,11 +549,11 @@ gnome_app_set_toolbar (GnomeApp *app,
 	g_return_if_fail(toolbar != NULL);
 	g_return_if_fail(app->toolbar == NULL);
 
-	hb = gtk_handle_box_new();
-	gtk_widget_show(hb);
-	app->toolbar = GTK_WIDGET(toolbar);
-	gtk_signal_connect(GTK_OBJECT(hb), "button_press_event",
-			   GTK_SIGNAL_FUNC(gnome_app_rightclick_toolbar), app);
+	hb = gtk_handle_box_new ();
+	gtk_widget_show (hb);
+	app->toolbar = GTK_WIDGET (toolbar);
+	gtk_signal_connect (GTK_OBJECT(hb), "button_press_event",
+			    GTK_SIGNAL_FUNC (gnome_app_rightclick_toolbar), app);
 	gtk_widget_show(app->toolbar);
 	gtk_container_add(GTK_CONTAINER(hb), app->toolbar);
 
