af36fb7a2eadc682ddd9330351675794fc50a989
Arturo Espinosa <unammx@src.gnome.org>
1998-07-07 16:10:27 +0000

    Oops!  It helps if you actually add the files... - Federico

---
+++ libgnomeui/gnome-canvas-image.c
@@ -0,0 +1,426 @@
+/* Image item type for GnomeCanvas widget
+ *
+ * GnomeCanvas is basically a port of the Tk toolkit's most excellent canvas widget.  Tk is
+ * copyrighted by the Regents of the University of California, Sun Microsystems, and other parties.
+ *
+ * Copyright (C) 1998 The Free Software Foundation
+ *
+ * Author: Federico Mena <federico@nuclecu.unam.mx>
+ */
+
+#include <config.h>
+#include <math.h>
+#include "gnome-canvas-image.h"
+
+
+enum {
+	ARG_0,
+	ARG_IMAGE,
+	ARG_X,
+	ARG_Y,
+	ARG_WIDTH,
+	ARG_HEIGHT,
+	ARG_ANCHOR
+};
+
+
+static void gnome_canvas_image_class_init (GnomeCanvasImageClass *class);
+static void gnome_canvas_image_init       (GnomeCanvasImage      *image);
+static void gnome_canvas_image_destroy    (GtkObject             *object);
+static void gnome_canvas_image_set_arg    (GtkObject             *object,
+					   GtkArg                *arg,
+					   guint                  arg_id);
+
+static void   gnome_canvas_image_reconfigure (GnomeCanvasItem *item);
+static void   gnome_canvas_image_realize     (GnomeCanvasItem *item);
+static void   gnome_canvas_image_unrealize   (GnomeCanvasItem *item);
+static void   gnome_canvas_image_draw        (GnomeCanvasItem *item, GdkDrawable *drawable,
+					      int x, int y, int width, int height);
+static double gnome_canvas_image_point       (GnomeCanvasItem *item, double x, double y,
+					      int cx, int cy, GnomeCanvasItem **actual_item);
+static void   gnome_canvas_image_translate   (GnomeCanvasItem *item, double dx, double dy);
+
+
+GtkType
+gnome_canvas_image_get_type (void)
+{
+	static GtkType image_type = 0;
+
+	if (!image_type) {
+		GtkTypeInfo image_info = {
+			"GnomeCanvasImage",
+			sizeof (GnomeCanvasImage),
+			sizeof (GnomeCanvasImageClass),
+			(GtkClassInitFunc) gnome_canvas_image_class_init,
+			(GtkObjectInitFunc) gnome_canvas_image_init,
+			NULL, /* reserved_1 */
+			NULL, /* reserved_2 */
+			(GtkClassInitFunc) NULL
+		};
+
+		image_type = gtk_type_unique (gnome_canvas_item_get_type (), &image_info);
+	}
+
+	return image_type;
+}
+
+static void
+gnome_canvas_image_class_init (GnomeCanvasImageClass *class)
+{
+	GtkObjectClass *object_class;
+	GnomeCanvasItemClass *item_class;
+
+	object_class = (GtkObjectClass *) class;
+	item_class = (GnomeCanvasItemClass *) class;
+
+	gtk_object_add_arg_type ("GnomeCanvasImage::image", GTK_TYPE_POINTER, GTK_ARG_WRITABLE, ARG_IMAGE);
+	gtk_object_add_arg_type ("GnomeCanvasImage::x", GTK_TYPE_DOUBLE, GTK_ARG_WRITABLE, ARG_X);
+	gtk_object_add_arg_type ("GnomeCanvasImage::y", GTK_TYPE_DOUBLE, GTK_ARG_WRITABLE, ARG_Y);
+	gtk_object_add_arg_type ("GnomeCanvasImage::width", GTK_TYPE_DOUBLE, GTK_ARG_WRITABLE, ARG_WIDTH);
+	gtk_object_add_arg_type ("GnomeCanvasImage::height", GTK_TYPE_DOUBLE, GTK_ARG_WRITABLE, ARG_HEIGHT);
+	gtk_object_add_arg_type ("GnomeCanvasImage::anchor", GTK_TYPE_ANCHOR_TYPE, GTK_ARG_WRITABLE, ARG_ANCHOR);
+
+	object_class->destroy = gnome_canvas_image_destroy;
+	object_class->set_arg = gnome_canvas_image_set_arg;
+
+	item_class->reconfigure = gnome_canvas_image_reconfigure;
+	item_class->realize = gnome_canvas_image_realize;
+	item_class->unrealize = gnome_canvas_image_unrealize;
+	item_class->draw = gnome_canvas_image_draw;
+	item_class->point = gnome_canvas_image_point;
+	item_class->translate = gnome_canvas_image_translate;
+}
+
+static void
+gnome_canvas_image_init (GnomeCanvasImage *image)
+{
+	image->x = 0.0;
+	image->y = 0.0;
+	image->width = 0.0;
+	image->height = 0.0;
+	image->anchor = GTK_ANCHOR_CENTER;
+}
+
+static void
+free_pixmap_and_mask (GnomeCanvasImage *image)
+{
+	if (image->pixmap)
+		gdk_imlib_free_pixmap (image->pixmap);
+
+	if (image->mask)
+		gdk_imlib_free_bitmap (image->mask);
+
+	image->pixmap = NULL;
+	image->mask = NULL;
+}
+
+static void
+gnome_canvas_image_destroy (GtkObject *object)
+{
+	g_return_if_fail (object != NULL);
+	g_return_if_fail (GNOME_IS_CANVAS_IMAGE (object));
+
+	free_pixmap_and_mask (GNOME_CANVAS_IMAGE (object));
+}
+
+static void
+recalc_bounds (GnomeCanvasImage *image)
+{
+	GnomeCanvasItem *item;
+	double wx, wy;
+
+	item = GNOME_CANVAS_ITEM (image);
+
+	/* Get world coordinates */
+
+	wx = image->x;
+	wy = image->y;
+	gnome_canvas_item_i2w (item, &wx, &wy);
+
+	/* Get canvas pixel coordinates */
+
+	gnome_canvas_w2c (item->canvas, wx, wy, &image->cx, &image->cy);
+
+	/* Anchor image */
+
+	switch (image->anchor) {
+	case GTK_ANCHOR_NW:
+	case GTK_ANCHOR_W:
+	case GTK_ANCHOR_SW:
+		break;
+
+	case GTK_ANCHOR_N:
+	case GTK_ANCHOR_CENTER:
+	case GTK_ANCHOR_S:
+		image->cx -= image->im ? (image->im->rgb_width / 2) : 0;
+		break;
+
+	case GTK_ANCHOR_NE:
+	case GTK_ANCHOR_E:
+	case GTK_ANCHOR_SE:
+		image->cx -= image->im ? image->im->rgb_width : 0;
+		break;
+	}
+
+	switch (image->anchor) {
+	case GTK_ANCHOR_NW:
+	case GTK_ANCHOR_N:
+	case GTK_ANCHOR_NE:
+		break;
+
+	case GTK_ANCHOR_W:
+	case GTK_ANCHOR_CENTER:
+	case GTK_ANCHOR_E:
+		image->cy -= image->im ? (image->im->rgb_height / 2) : 0;
+		break;
+
+	case GTK_ANCHOR_SW:
+	case GTK_ANCHOR_S:
+	case GTK_ANCHOR_SE:
+		image->cy -= image->im ? image->im->rgb_height : 0;
+		break;
+	}
+
+	/* Bounds */
+
+	item->x1 = image->cx;
+	item->y1 = image->cy;
+	item->x2 = image->cx + (image->im ? image->im->rgb_width : 0);
+	item->y2 = image->cy + (image->im ? image->im->rgb_height : 0);
+
+	gnome_canvas_group_child_bounds (GNOME_CANVAS_GROUP (item->parent), item);
+}
+
+static void
+gnome_canvas_image_set_arg (GtkObject *object, GtkArg *arg, guint arg_id)
+{
+	GnomeCanvasItem *item;
+	GnomeCanvasImage *image;
+	int update;
+	int calc_bounds;
+
+	item = GNOME_CANVAS_ITEM (object);
+	image = GNOME_CANVAS_IMAGE (object);
+
+	update = FALSE;
+
+	switch (arg_id) {
+	case ARG_IMAGE:
+		/* The pixmap and mask will be freed when the item is reconfigured */
+		image->im = GTK_VALUE_POINTER (*arg);
+		if (image->im) {
+			image->width = image->im->rgb_width / item->canvas->pixels_per_unit;
+			image->height = image->im->rgb_height / item->canvas->pixels_per_unit;
+		} else
+			image->width = image->height = 0.0;
+
+		update = TRUE;
+		calc_bounds = TRUE;
+		break;
+
+	case ARG_X:
+		image->x = GTK_VALUE_DOUBLE (*arg);
+		calc_bounds = TRUE;
+		break;
+
+	case ARG_Y:
+		image->y = GTK_VALUE_DOUBLE (*arg);
+		calc_bounds = TRUE;
+		break;
+
+	case ARG_WIDTH:
+		image->width = GTK_VALUE_DOUBLE (*arg);
+		update = TRUE;
+		calc_bounds = TRUE;
+		break;
+
+	case ARG_HEIGHT:
+		image->height = GTK_VALUE_DOUBLE (*arg);
+		update = TRUE;
+		calc_bounds = TRUE;
+		break;
+
+	case ARG_ANCHOR:
+		image->anchor = GTK_VALUE_ENUM (*arg);
+		update = TRUE;
+		calc_bounds = TRUE;
+		break;
+
+	default:
+		break;
+	}
+
+	if (update)
+		(* GNOME_CANVAS_ITEM_CLASS (item->object.klass)->reconfigure) (item);
+
+	if (calc_bounds)
+		recalc_bounds (image);
+}
+
+static void
+gnome_canvas_image_reconfigure (GnomeCanvasItem *item)
+{
+	GnomeCanvasImage *image;
+
+	image = GNOME_CANVAS_IMAGE (item);
+
+	free_pixmap_and_mask (image);
+
+	if (image->im) {
+		image->im->rgb_width = image->width * item->canvas->pixels_per_unit;
+		image->im->rgb_height = image->height * item->canvas->pixels_per_unit;
+
+		gdk_imlib_render (image->im, image->im->rgb_width, image->im->rgb_height);
+
+		image->pixmap = gdk_imlib_move_image (image->im);
+		g_assert (image->pixmap != NULL);
+		image->mask = gdk_imlib_move_mask (image->im);
+
+		if (image->gc)
+			gdk_gc_set_clip_mask (image->gc, image->mask);
+	}
+
+	recalc_bounds (image);
+}
+
+static void
+gnome_canvas_image_realize (GnomeCanvasItem *item)
+{
+	GnomeCanvasImage *image;
+
+	image = GNOME_CANVAS_IMAGE (item);
+
+	image->gc = gdk_gc_new (GTK_WIDGET (item->canvas)->window);
+	(* GNOME_CANVAS_ITEM_CLASS (item->object.klass)->reconfigure) (item);
+}
+
+static void
+gnome_canvas_image_unrealize (GnomeCanvasItem *item)
+{
+	GnomeCanvasImage *image;
+
+	image = GNOME_CANVAS_IMAGE (item);
+
+	gdk_gc_unref (image->gc);
+}
+
+static void
+gnome_canvas_image_draw (GnomeCanvasItem *item, GdkDrawable *drawable,
+			 int x, int y, int width, int height)
+{
+	GnomeCanvasImage *image;
+
+	image = GNOME_CANVAS_IMAGE (item);
+
+	if (!image->im)
+		return;
+
+	if (image->mask)
+		gdk_gc_set_clip_origin (image->gc, image->cx - x, image->cy - y);
+
+	gdk_draw_pixmap (drawable,
+			 image->gc,
+			 image->pixmap,
+			 0, 0,
+			 image->cx - x,
+			 image->cy - y,
+			 image->im->rgb_width,
+			 image->im->rgb_height);
+}
+
+static double
+gnome_canvas_image_point (GnomeCanvasItem *item, double x, double y,
+			  int cx, int cy, GnomeCanvasItem **actual_item)
+{
+	GnomeCanvasImage *image;
+	double x1, y1, x2, y2;
+	double dx, dy;
+
+	image = GNOME_CANVAS_IMAGE (item);
+
+	*actual_item = item;
+
+	/* FIXME: really, we should be taking the mask into account.  I am too lazy to figure it out
+	 * right now, so sue me.
+	 */
+
+	x1 = image->x;
+	y1 = image->y;
+
+	switch (image->anchor) {
+	case GTK_ANCHOR_NW:
+	case GTK_ANCHOR_W:
+	case GTK_ANCHOR_SW:
+		break;
+
+	case GTK_ANCHOR_N:
+	case GTK_ANCHOR_CENTER:
+	case GTK_ANCHOR_S:
+		x1 -= image->width / 2.0;
+		break;
+
+	case GTK_ANCHOR_NE:
+	case GTK_ANCHOR_E:
+	case GTK_ANCHOR_SE:
+		x1 -= image->width;
+		break;
+	}
+
+	switch (image->anchor) {
+	case GTK_ANCHOR_NW:
+	case GTK_ANCHOR_N:
+	case GTK_ANCHOR_NE:
+		break;
+
+	case GTK_ANCHOR_W:
+	case GTK_ANCHOR_CENTER:
+	case GTK_ANCHOR_E:
+		y1 -= image->height / 2.0;
+		break;
+
+	case GTK_ANCHOR_SW:
+	case GTK_ANCHOR_S:
+	case GTK_ANCHOR_SE:
+		y1 -= image->height;
+		break;
+	}
+
+	x2 = x1 + image->width;
+	y2 = y1 + image->height;
+
+	/* Is point inside image? */
+
+	if ((x >= x1) && (y >= y1) && (x <= x2) && (y <= y2))
+		return 0.0;
+
+	/* Point is outside image */
+
+	if (x < x1)
+		dx = x1 - x;
+	else if (x > x2)
+		dx = x - x2;
+	else
+		dx = 0.0;
+
+	if (y < y1)
+		dy = y1 - y;
+	else if (y > y2)
+		dy = y - y2;
+	else
+		dy = 0.0;
+
+	return sqrt (dx * dx + dy * dy);
+}
+
+static void
+gnome_canvas_image_translate (GnomeCanvasItem *item, double dx, double dy)
+{
+	GnomeCanvasImage *image;
+
+	image = GNOME_CANVAS_IMAGE (item);
+
+	image->x += dx;
+	image->y += dy;
+
+	recalc_bounds (image);
+}
---
+++ libgnomeui/gnome-canvas-image.h
@@ -0,0 +1,62 @@
+/* Image item type for GnomeCanvas widget
+ *
+ * GnomeCanvas is basically a port of the Tk toolkit's most excellent canvas widget.  Tk is
+ * copyrighted by the Regents of the University of California, Sun Microsystems, and other parties.
+ *
+ * Copyright (C) 1998 The Free Software Foundation
+ *
+ * Author: Federico Mena <federico@nuclecu.unam.mx>
+ */
+
+#ifndef GNOME_CANVAS_IMAGE_H
+#define GNOME_CANVAS_IMAGE_H
+
+#include <libgnome/gnome-defs.h>
+#include <gtk/gtkpacker.h> /* why the hell is GtkAnchorType here and not in gtkenums.h? */
+#include <gdk_imlib.h>
+#include "gnome-canvas.h"
+
+
+BEGIN_GNOME_DECLS
+
+
+#define GNOME_TYPE_CANVAS_IMAGE            (gnome_canvas_image_get_type ())
+#define GNOME_CANVAS_IMAGE(obj)            (GTK_CHECK_CAST ((obj), GNOME_TYPE_CANVAS_IMAGE, GnomeCanvasImage))
+#define GNOME_CANVAS_IMAGE_CLASS(klass)    (GTK_CHECK_CLASS_CAST ((klass), GNOME_TYPE_CANVAS_IMAGE, GnomeCanvasImageClass))
+#define GNOME_IS_CANVAS_IMAGE(obj)         (GTK_CHECK_TYPE ((obj), GNOME_TYPE_CANVAS_IMAGE))
+#define GNOME_IS_CANVAS_IMAGE_CLASS(klass) (GTK_CHECK_CLASS_TYPE ((klass), GNOME_TYPE_CANVAS_IMAGE))
+
+
+typedef struct _GnomeCanvasImage GnomeCanvasImage;
+typedef struct _GnomeCanvasImageClass GnomeCanvasImageClass;
+
+struct _GnomeCanvasImage {
+	GnomeCanvasItem item;
+
+	GdkImlibImage *im;		/* The image to paint */
+	GdkPixmap *pixmap;		/* Pixmap rendered from the image */
+	GdkBitmap *mask;		/* Mask rendered from the image */
+
+	double x, y;			/* Position at anchor */
+	double width, height;		/* Size of image in units */
+	GtkAnchorType anchor;		/* Anchor side for image */
+
+	int cx, cy;			/* Top-left canvas coordinates for display */
+	GdkGC *gc;			/* GC for drawing image */
+};
+
+struct _GnomeCanvasImageClass {
+	GnomeCanvasItemClass parent_class;
+};
+
+
+/* Standard Gtk function */
+GtkType gnome_canvas_image_get_type (void);
+
+/* Notifies an image item that its imlib image has changed and must be re-rendered */
+void gnome_canvas_image_update (GnomeCanvasImage *image);
+
+
+END_GNOME_DECLS
+
+#endif
