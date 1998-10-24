c13d95d8315e0e9ed463c26e56899e3bd8ad2345
David Abilleira <odaf@nexo.es>
1998-10-24 22:23:19 +0000

    Now it uses GNOME_STOCK_PIXMAP_FONT instead of the one in
    1998-10-24  David Abilleira  <odaf@nexo.es>
    
    	* gnome-font-picker.c: Now it uses GNOME_STOCK_PIXMAP_FONT instead of
            the one in gnome-font-picer-ico.png
    
    	* gnome-font-picker-ico.png: Removed
    	* gnome-font-picker-ico.h: Removed

--- libgnomeui/gnome-font-picker.c
+++ libgnomeui/gnome-font-picker.c
@@ -27,6 +27,7 @@
 #include <gtk/gtkfontsel.h>
 #include <gdk_imlib.h>
 #include <libgnomeui/gnome-pixmap.h>
+#include <libgnomeui/gnome-stock.h>
 #include "gnome-font-picker.h"
 #include <libgnome/gnome-i18n.h>
 
@@ -521,12 +522,7 @@ GtkWidget *gnome_font_picker_create_inside(GnomeFontPicker *gfp)
     GtkWidget *widget;
     
     if (gfp->mode==GNOME_FONT_PICKER_MODE_PIXMAP) {
-        widget = (GtkWidget *)gnome_pixmap_new_from_rgb_d_shaped(
-                  (unsigned char *)gnome_font_picker_ico,
-                  NULL,
-                  gnome_font_picker_ico_width,
-                  gnome_font_picker_ico_height,
-                  (GdkImlibColor *) &gnome_font_picker_ico_alpha);
+        widget=gnome_stock_new_with_icon(GNOME_STOCK_PIXMAP_FONT);
         gtk_widget_show(widget);
     }
     else if (gfp->mode==GNOME_FONT_PICKER_MODE_FONT_INFO) {
