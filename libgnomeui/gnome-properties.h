817daab097279d350a80ec8aa4a20e33f82d1275
Martin Baulig <martin@home-of-linux.org>
1998-11-27 16:00:13 +0000

    Scratched it. Please do not remove this two files from CVS and the
    1998-11-27  Martin Baulig  <martin@home-of-linux.org>
    
    	* gnome-properties.[ch]: Scratched it. Please do not remove this
    	two files from CVS and the Makefile.am at the moment since I want
    	to add some functions here.

--- libgnomeui/gnome-properties.h
+++ libgnomeui/gnome-properties.h
@@ -4,30 +4,6 @@
 
 BEGIN_GNOME_DECLS
 
-typedef struct {
-	GtkWidget *property_box;
-
-	GList *props;
-} GnomePropertyConfigurator;
-
-/* This is the first parameter to the callback function */
-typedef enum {
-	GNOME_PROPERTY_READ,
-	GNOME_PROPERTY_WRITE,
-	GNOME_PROPERTY_APPLY,
-	GNOME_PROPERTY_SETUP
-} GnomePropertyRequest;
-
-GnomePropertyConfigurator *gnome_property_configurator_new (void);
-  
-void gnome_property_configurator_destroy (GnomePropertyConfigurator *);
-void gnome_property_configurator_register (GnomePropertyConfigurator *, 
-					   int (*callback)(GnomePropertyRequest));
-void gnome_property_configurator_setup (GnomePropertyConfigurator *);
-
-void gnome_property_configurator_request_foreach (GnomePropertyConfigurator *th,
-						  GnomePropertyRequest r);
-
 END_GNOME_DECLS
 
 #endif
