d923bbb6594fc8870fb7f368e18fcd9c49e16788
Martin Baulig <martin@src.gnome.org>
1998-11-29 13:17:06 +0000

    Reverted all my changes here until I have time to fix all the code that depends upon this.

--- libgnomeui/gnome-properties.h
+++ libgnomeui/gnome-properties.h
@@ -1,3 +1,35 @@
-/* This file is currently empty; I'm going to add new code here soon.
-   Oct 27 1998. Martin
- */
+#ifndef GNOME_PROPERTIES_H
+#define GNOME_PROPERTIES_H
+#include <libgnome/gnome-defs.h>
+
+BEGIN_GNOME_DECLS
+
+typedef struct {
+	GtkWidget *notebook;
+
+	GList *props;
+} GnomePropertyConfigurator;
+
+/* This is the first parameter to the callback function */
+typedef enum {
+	GNOME_PROPERTY_READ,
+	GNOME_PROPERTY_WRITE,
+	GNOME_PROPERTY_APPLY,
+	GNOME_PROPERTY_SETUP
+} GnomePropertyRequest;
+
+GnomePropertyConfigurator
+     *gnome_property_configurator_new (void);
+  
+void gnome_property_configurator_destroy (GnomePropertyConfigurator *);
+void gnome_property_configurator_register (GnomePropertyConfigurator *, 
+					   int (*callback)(GnomePropertyRequest));
+void gnome_property_configurator_setup (GnomePropertyConfigurator *);
+gint gnome_property_configurator_request (GnomePropertyConfigurator *,
+					  GnomePropertyRequest);
+void gnome_property_configurator_request_foreach (GnomePropertyConfigurator *th,
+						  GnomePropertyRequest r);
+
+END_GNOME_DECLS
+
+#endif
