74811dbcb4d25ab21bce057e3f5c0c807fce402c
James Henstridge <james@daa.com.au>
1998-12-08 04:06:35 +0000

    added documentation for this widget.
    1998-12-08  James Henstridge  <james@daa.com.au>
    
    	* gnome-mdi-generic-child.c: added documentation for this widget.

--- libgnomeui/gnome-mdi-generic-child.c
+++ libgnomeui/gnome-mdi-generic-child.c
@@ -103,6 +103,18 @@ static void gnome_mdi_generic_child_init (GnomeMDIGenericChild *child)
 	child->get_config_string_data = NULL;
 }
 
+/**
+ * gnome_mdi_generic_child_new:
+ * @name: the name of this MDI child.
+ * 
+ * Creates a new mdi child, which has the ability to set view creators, etc
+ * on an instance basis (rather than on a class basis like &GnomeMDIChild).
+ *
+ * After creation, you will need to set, at a minimum, the view creator
+ * function.
+ * 
+ * Return value: A newly created &GnomeMDIGenericChild object.
+ **/
 GnomeMDIGenericChild *gnome_mdi_generic_child_new (gchar *name)
 {
 	GnomeMDIGenericChild *child;
@@ -114,6 +126,18 @@ GnomeMDIGenericChild *gnome_mdi_generic_child_new (gchar *name)
 	return child;
 }
 
+/**
+ * gnome_mdi_generic_child_set_view_creator:
+ * @child: the mdi child object
+ * @func: a function used to create views
+ * @data: optional user data.
+ * 
+ * This function sets the function that is used to create new views for this
+ * particular mdi child object.  The function should return a newly created
+ * widget (the view).
+ *
+ * A &GnomeMDIGenericChild must have a view creator.
+ **/
 void gnome_mdi_generic_child_set_view_creator (GnomeMDIGenericChild *child,
 											   GnomeMDIChildViewCreator func,
 											   gpointer data)
@@ -121,6 +145,22 @@ void gnome_mdi_generic_child_set_view_creator (GnomeMDIGenericChild *child,
         gnome_mdi_generic_child_set_view_creator_full(child,func,NULL,data,NULL);
 }
 
+/**
+ * gnome_mdi_generic_child_set_view_creator_full:
+ * @child: the mdi child object
+ * @func: a function to create views (not used if @marshal != %NULL)
+ * @marshal: a callback marshaller
+ * @data: optional user data
+ * @notify: a function used to free the user data.
+ * 
+ * Similar to gnome_mdi_generic_child_set_view_creator(), except that it gives
+ * more control to the programmer.  If @marshal is not %NULL, then it will
+ * be called instead of @func.
+ *
+ * The &GtkArg array passed to @marshal will be of length 2.  The first
+ * element will be @child, and the second is the return value (a pointer to
+ * a &GtkWidget).
+ **/
 void gnome_mdi_generic_child_set_view_creator_full (GnomeMDIGenericChild *child,
 												    GnomeMDIChildViewCreator func,
 													GtkCallbackMarshal marshal,
@@ -135,6 +175,17 @@ void gnome_mdi_generic_child_set_view_creator_full (GnomeMDIGenericChild *child,
 	child->create_view_dn = notify;
 }
 
+/**
+ * gnome_mdi_generic_child_set_menu_creator:
+ * @child: the mdi child object
+ * @func: a function to create a list of child specific menus
+ * @data: optional user data
+ * 
+ * Sets the function used to create child specific menus.  The function will
+ * should return a &GList of the menus created.
+ *
+ * A &GnomeMDIGenericChild doesn't require a menu creator.
+ **/
 void gnome_mdi_generic_child_set_menu_creator (GnomeMDIGenericChild *child,
 											   GnomeMDIChildMenuCreator func,
 											   gpointer data)
@@ -142,6 +193,22 @@ void gnome_mdi_generic_child_set_menu_creator (GnomeMDIGenericChild *child,
 	gnome_mdi_generic_child_set_menu_creator_full(child,func,NULL,data,NULL);
 }
 
+/**
+ * gnome_mdi_generic_child_set_menu_creator_full:
+ * @child: the mdi child object
+ * @func: a menu creator function (not used if @marshal != %NULL)
+ * @marshal: a callback marshaller
+ * @data: optional user data
+ * @notify: a destroy notify for the data
+ * 
+ * This function is similar to gnome_mdi_generic_child_set_menu_creator(),
+ * but gives extra flexibility to the programmer, in the form of a a
+ * destroy notify for the user data, and a callback marshaller.
+ *
+ * The &GtkArg array passed to @marshal is of length 3.  The first element
+ * will be @child, the second will be a view of @child, and the third is the
+ * return value (a pointer to the returned GList).
+ **/
 void gnome_mdi_generic_child_set_menu_creator_full (GnomeMDIGenericChild *child,
 													GnomeMDIChildMenuCreator func,
 													GtkCallbackMarshal marshal,
@@ -156,6 +223,16 @@ void gnome_mdi_generic_child_set_menu_creator_full (GnomeMDIGenericChild *child,
 	child->create_menus_dn = notify;
 }
 
+/**
+ * gnome_mdi_generic_child_set_config_func:
+ * @child: the mdi child object
+ * @func: a function to set the config key for session saves
+ * @data: optional user data
+ * 
+ * Sets the function used to get the config key used for session saves.
+ *
+ * A &GnomeMDIGenericChild doesn't require a config func.
+ **/
 void gnome_mdi_generic_child_set_config_func (GnomeMDIGenericChild *child,
 											  GnomeMDIChildConfigFunc func,
 											  gpointer data)
@@ -163,6 +240,20 @@ void gnome_mdi_generic_child_set_config_func (GnomeMDIGenericChild *child,
 	gnome_mdi_generic_child_set_config_func_full(child,func,NULL,data,NULL);
 }
 
+/**
+ * gnome_mdi_generic_child_set_config_func_full:
+ * @child: the mdi child object
+ * @func: a function (not used if @marshal != %NULL)
+ * @marshal: a callback marshaller
+ * @data: optional user data
+ * @notify: a destroy notify for the user data
+ * 
+ * A function similar to gnome_mdi_generic_child_set_config_func(), except
+ * it gives more control to the programmer.
+ *
+ * The &GtkArg array passed to @marshal is of length 2.  The first element is
+ * @child, and the second is the return value (a pointer to a string).
+ **/
 void gnome_mdi_generic_child_set_config_func_full (GnomeMDIGenericChild *child,
 												   GnomeMDIChildConfigFunc func,
 												   GtkCallbackMarshal marshal,
@@ -177,6 +268,19 @@ void gnome_mdi_generic_child_set_config_func_full (GnomeMDIGenericChild *child,
 	child->get_config_string_dn = notify;
 }
 
+/**
+ * gnome_mdi_generic_child_set_label_func:
+ * @child: a mdi child object
+ * @func: a function
+ * @data: optional user data
+ * 
+ * Sets the function used to set (or modify) the label for @child.  The
+ * first argument to @func is @child.  If a label exists, it will be passed
+ * to @func as the second argument, otherwise, %NULL is passed.  The
+ * function should return the modified label.
+ *
+ * A &GnomeMDIGenericChild doesn't require a label function.
+ **/
 void gnome_mdi_generic_child_set_label_func (GnomeMDIGenericChild *child,
 											 GnomeMDIChildLabelFunc func,
 											 gpointer data)
@@ -184,11 +288,27 @@ void gnome_mdi_generic_child_set_label_func (GnomeMDIGenericChild *child,
 	gnome_mdi_generic_child_set_label_func_full(child,func,NULL,data,NULL);
 }
 
+/**
+ * gnome_mdi_generic_child_set_label_func_full:
+ * @child: the mdi child object
+ * @func: a function (not used if @marshal != %NULL)
+ * @marshal: a callback marshaller
+ * @data: optional user data
+ * @notify: a destroy notify for the data
+ * 
+ * Similar to gnome_mdi_generic_child_set_label_func(), except it gives more
+ * flexibility to the programmer.
+ *
+ * The &GtkArg array passed to @marshal is of length 3.  The first argument
+ * is @child, the second is the old widget (or %NULL), and the third is the
+ * return value (a pointer to a &GtkWidget).
+ **/
 void gnome_mdi_generic_child_set_label_func_full (GnomeMDIGenericChild *child,
 												  GnomeMDIChildLabelFunc func,
 												  GtkCallbackMarshal marshal,
 												  gpointer data,
-												  GtkDestroyNotify notify) {
+												  GtkDestroyNotify notify)
+{
 	if(child->set_label_dn)
 		child->set_label_dn(child->set_label_data);
 	child->set_label = func;
