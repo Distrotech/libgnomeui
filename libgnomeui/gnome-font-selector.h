9b11f886f4eb936f636f102c9a53430e87577635
Gregory McLean <gregm@comstar.net>
1998-12-12 02:47:17 +0000

    FSF address update.. (Had the old Mass Ave addy)
    Fri Dec 11 21:45:32 1998 Gregory McLean <gregm@comstar.net>
    
            * *.[c|h] : FSF address update.. (Had the old Mass Ave addy)
    
    Weee happy downloading!

--- libgnomeui/gnome-font-selector.h
+++ libgnomeui/gnome-font-selector.h
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Foundation, Inc., 59 Temple Place - Suite 330, Cambridge, MA 02139, USA.
  */
 
 #ifndef __GNOME_FONT_SELECTOR_H__
