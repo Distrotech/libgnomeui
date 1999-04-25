e9103c6b47a2d17d84659eacbdec7f9365af3095
Arturo Espinosa <unammx@src.gnome.org>
1999-04-25 20:28:59 +0000

    Add missing file

---
+++ libgnomeui/gnome-compat.h
@@ -0,0 +1,15 @@
+/*
+ * Source code compatibility functions for GNOME
+ *
+ * (C) 1999 the Free Software Foundation
+ */
+
+#ifndef GNOME_COMPAT_H
+#define GNOME_COMPAT_H
+
+#ifndef GNOME_DISABLE_COMPAT_H
+
+#  define GNOME_IS_MDI_MDI_CHILD     GNOME_IS_MDI_GENERIC_CHILD
+
+#endif /* GNOME_DISABLE_COMPAT_H */
+#endif
