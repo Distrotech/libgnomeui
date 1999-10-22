b1b620cf5fa2cf0a41c986ee99cea5c23361a320
Jonathan Blandford <jrb@redhat.com>
1999-10-22 19:01:01 +0000

    Add support for --enable-gnome-compat (off by default) to let the compat
    1999-10-22  Jonathan Blandford  <jrb@redhat.com>
    
    	* configure.in (GNOME_IDLDIR): Add support for
    	--enable-gnome-compat (off by default) to let the compat library
    	be installed.

---
+++ libgnomeui/libgnomeui10compat.h
@@ -0,0 +1,11 @@
+#ifndef LIBGNOMEUI10COMPAT_H
+#define LIBGNOMEUI10COMPAT_H
+
+#include <libgnomeui/compat/gnome-font-selector.h>
+#include <libgnomeui/compat/gnome-guru.h>
+#include <libgnomeui/compat/gnome-spell.h>
+#include <libgnomeui/compat/gnome-startup.h>
+#include <libgnomeui/compat/gtkcauldron.h>
+#include <libgnomeui/compat/gnomeui10-compat.h>
+
+#endif 
