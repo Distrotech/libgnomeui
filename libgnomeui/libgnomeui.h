c7fdb85bf1ded027dd17e3fe50aa6ff00627b560
Martin Baulig <baulig@suse.de>
2001-06-12 13:46:13 +0000

    Removed.
    2001-06-12  Martin Baulig  <baulig@suse.de>
    
    	* gnome-gconf.[ch]: Removed.

--- libgnomeui/libgnomeui.h
+++ libgnomeui/libgnomeui.h
@@ -45,7 +45,6 @@
 #include <libgnomeui/gnome-image-entry.h>
 #include <libgnomeui/gnome-image-selector.h>
 #include <libgnomeui/gnome-font-picker.h>
-#include <libgnomeui/gnome-gconf.h>
 #include <libgnomeui/gnome-geometry.h>
 
 #include <libgnomeui/gnome-icon-list.h>
