096cb494e147cf7990c81381264b7c8ca51cc951
Peter Williams <peterw@ximian.com>
2001-07-15 00:08:08 +0000

    Add a REBUILD rule as in libgnome.
    2001-07-08  Peter Williams  <peterw@ximian.com>
    
    	* configure.in (REBUILD): Add a REBUILD rule as in libgnome.
    
    In libgnomeui:
    
    2001-07-08  Peter Williams  <peterw@ximian.com>
    
    	* Makefile.am ($(srcdir)/gnome.defs): Add $(srcdir)/
    	to these targets for when srcdir != builddir.
    	(gnometypebuiltins.c,h): Change from gnome-maketypes.awk
    	to the glib-mkenums script.
    	(gnometype_inits.c): But also provide hooks for one function
    	that will init all the types.
    
    	* gnometypes.c (gnome_type_init): #include gnometype_inits.c
    	to initialize all of the types.

--- libgnomeui/gnometypes.c
+++ libgnomeui/gnometypes.c
@@ -2,46 +2,15 @@
 #include <gtk/gtktypeutils.h>
 #include <libgnomeui.h>
 
-#include "gnometypebuiltins_vars.c"
-#include "gnometypebuiltins_evals.c"
-void gnome_type_init(void);
+void gnome_type_init (void);
 
-void
-gnome_type_init(void) {
-  static gboolean initialized = FALSE;
+void gnome_type_init (void) {
+	static gboolean initialized = FALSE;
 
-  if (!initialized) {
-    int i;
+	if (initialized)
+		return;
 
-    static struct {
-      gchar           *type_name;
-      GtkType         *type_id;
-      GtkType          parent;
-      gconstpointer    pointer1;
-      gconstpointer    pointer2;
-      gconstpointer    pointer3;
-      gboolean         boolean1;
-    } builtin_info[GNOME_TYPE_NUM_BUILTINS + 1] = {
-#include "gnometypebuiltins_ids.c"
-      { NULL }
-    };
+#include "gnometype_inits.c"
 
-    initialized = TRUE;
-
-    for (i = 0; i < GNOME_TYPE_NUM_BUILTINS; i++)
-      {
-	GtkType type_id = GTK_TYPE_INVALID;
-	g_assert (builtin_info[i].type_name != NULL);
-	if ( builtin_info[i].parent == GTK_TYPE_ENUM )
-	  type_id = g_enum_register_static (builtin_info[i].type_name, (GtkEnumValue *)builtin_info[i].pointer1);
-	else if ( builtin_info[i].parent == GTK_TYPE_FLAGS )
-	  type_id = g_flags_register_static (builtin_info[i].type_name, (GtkFlagValue *)builtin_info[i].pointer1);
-	else if ( builtin_info[i].parent == GTK_TYPE_BOXED )
-	  type_id = g_boxed_type_register_static (builtin_info[i].type_name, (GBoxedInitFunc)builtin_info[i].pointer1, (GBoxedCopyFunc)builtin_info[i].pointer2, (GBoxedFreeFunc)builtin_info[i].pointer3, builtin_info[1].boolean1);
-
-	g_assert (type_id != GTK_TYPE_INVALID);
-	(*builtin_info[i].type_id) = type_id;
-      }
-  }
+	initialized = TRUE;
 }
-
