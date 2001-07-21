1735a385b0fea87ef6612f6663c2e1c249786ca0
Martin Baulig <baulig@suse.de>
2001-07-21 20:08:32 +0000

    Removed all icons which are already in GTK+: stock_add.png,
    2001-07-21  Martin Baulig  <baulig@suse.de>
    
    	* pixmaps/: Removed all icons which are already in GTK+:
    	stock_add.png, stock_align_center.png, stock_align_justify.png,
    	stock_align_left.png, stock_align_right.png, stock_bottom.png,
    	stock_button_apply.png, stock_button_cancel.png, stock_button_close.png,
    	stock_button_no.png, stock_button_ok.png, stock_button_yes.png, stock_cdrom.png,
    	stock_clear.png, stock_close.png, stock_colorselector.png, stock_convert.png,
    	stock_copy.png, stock_cut.png, stock_down_arrow.png, stock_exec.png,
    	stock_exit.png, stock_first.png, stock_font.png, stock_help.png, stock_home.png,
    	stock_index.png, stock_jump_to.png, stock_last.png, stock_left_arrow.png,
    	stock_new.png, stock_open.png, stock_paste.png, stock_preferences.png,
    	stock_print.png, stock_properties.png, stock_redo.png, stock_refresh.png,
    	stock_remove.png, stock_revert.png, stock_right_arrow.png, stock_save.png,
    	stock_save_as.png, stock_search.png, stock_search_replace.png,
    	stock_spellcheck.png, stock_stop.png, stock_text_bold.png, stock_text_italic.png,
    	stock_text_strikeout.png, stock_text_underline.png, stock_top.png,
    	stock_trash.png, stock_undelete.png, stock_undo.png, stock_up_arrow.png.

--- libgnomeui/gnome-stock-icons.c
+++ libgnomeui/gnome-stock-icons.c
@@ -93,172 +93,66 @@ get_default_icons (GtkIconFactory *factory)
 
     gnome_icon_size_toolbar = gtk_icon_size_register ("gnome-toolbar", 20, 20);
 
-    add_sized (factory, stock_new, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_NEW);
-    add_sized (factory, stock_save, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_SAVE);
-    add_sized (factory, stock_save_as, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_SAVE_AS);
-    add_sized (factory, stock_revert, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_REVERT);
-    add_sized (factory, stock_cut, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_CUT);
-    add_sized (factory, stock_help, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_HELP);
-    add_sized (factory, stock_print, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_PRINT);
-    add_sized (factory, stock_search, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_SEARCH);
-    add_sized (factory, stock_search_replace, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_SRCHRPL);
-    add_sized (factory, stock_left_arrow, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_BACK);
-    add_sized (factory, stock_right_arrow, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_FORWARD);
-    add_sized (factory, stock_first, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_FIRST);
-    add_sized (factory, stock_last, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_LAST);
-    add_sized (factory, stock_home, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_HOME);
-    add_sized (factory, stock_stop, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_STOP);
-    add_sized (factory, stock_refresh, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_REFRESH);
-    add_sized (factory, stock_undelete, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_UNDELETE);
-    add_sized (factory, stock_open, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_OPEN);
-    add_sized (factory, stock_close, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_CLOSE);
-    add_sized (factory, stock_copy, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_COPY);
-    add_sized (factory, stock_paste, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_PASTE);
-    add_sized (factory, stock_properties, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_PROPERTIES);
-    add_sized (factory, stock_preferences, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_PREFERENCES);
-    add_sized (factory, stock_undo, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_UNDO);
-    add_sized (factory, stock_redo, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_REDO);
     add_sized (factory, stock_timer, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_TIMER);
-    add_sized (factory, stock_timer_stopped, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_TIMER_STOP);
+    
     add_sized (factory, stock_mail, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_MAIL);
     add_sized (factory, stock_mail_receive, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_MAIL_RCV);
     add_sized (factory, stock_mail_send, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_MAIL_SND);
     add_sized (factory, stock_mail_reply, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_MAIL_RPL);
     add_sized (factory, stock_mail_forward, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_MAIL_FWD);
-    add_sized (factory, stock_mail_compose, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_MAIL_NEW);
-    add_sized (factory, stock_trash, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_TRASH);
-    add_sized (factory, stock_trash_full, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_TRASH_FULL);
-    add_sized (factory, stock_spellcheck, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_SPELLCHECK);
+    
     add_sized (factory, stock_mic, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_MIC);
     add_sized (factory, stock_volume, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_VOLUME);
     add_sized (factory, stock_midi, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_MIDI);
     add_sized (factory, stock_line_in, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_LINE_IN);
-    add_sized (factory, stock_cdrom, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_CDROM);
+    
     add_sized (factory, stock_book_red, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_BOOK_RED);
     add_sized (factory, stock_book_green, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_BOOK_GREEN);
     add_sized (factory, stock_book_blue, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_BOOK_BLUE);
     add_sized (factory, stock_book_yellow, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_BOOK_YELLOW);
-    add_sized (factory, stock_book_open, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_BOOK_OPEN);
-    add_sized (factory, stock_convert, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_CONVERT);
-    add_sized (factory, stock_jump_to, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_JUMP_TO);
-    add_sized (factory, stock_up_arrow, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_UP);
-    add_sized (factory, stock_down_arrow, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_DOWN);
-    add_sized (factory, stock_top, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_TOP);
-    add_sized (factory, stock_bottom, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_BOTTOM);
+    
     add_sized (factory, stock_attach, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_ATTACH);
-    add_sized (factory, stock_font, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_FONT);
-    add_sized (factory, stock_index, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_INDEX);
-    add_sized (factory, stock_exec, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_EXEC);
-    add_sized (factory, stock_align_left, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_ALIGN_LEFT);
-    add_sized (factory, stock_align_right, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_ALIGN_RIGHT);
-    add_sized (factory, stock_align_center, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_ALIGN_CENTER);
-    add_sized (factory, stock_align_justify, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_ALIGN_JUSTIFY);
-    add_sized (factory, stock_text_bold, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_TEXT_BOLD);
-    add_sized (factory, stock_text_italic, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_TEXT_ITALIC);
-    add_sized (factory, stock_text_underline, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_TEXT_UNDERLINE);
-    add_sized (factory, stock_text_strikeout, GTK_ICON_SIZE_BUTTON, GNOME_STOCK_PIXMAP_TEXT_STRIKEOUT);
+    
     add_sized (factory, stock_not, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_NOT);
     add_sized (factory, stock_scores, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_SCORES);
-    add_sized (factory, stock_exit, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_EXIT);
+    
     add_sized (factory, stock_menu_about, GTK_ICON_SIZE_MENU, GNOME_STOCK_PIXMAP_ABOUT);
-    add_sized (factory, stock_save, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_SAVE);
-    add_sized (factory, stock_save_as, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_SAVE_AS);
-    add_sized (factory, stock_revert, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_REVERT);
-    add_sized (factory, stock_open, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_OPEN);
-    add_sized (factory, stock_close, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_CLOSE);
-    add_sized (factory, stock_exit, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_QUIT);
-    add_sized (factory, stock_cut, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_CUT);
-    add_sized (factory, stock_copy, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_COPY);
-    add_sized (factory, stock_paste, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_PASTE);
-    add_sized (factory, stock_properties, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_PROP);
-    add_sized (factory, stock_preferences, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_PREF);
-    add_sized (factory, stock_undo, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_UNDO);
-    add_sized (factory, stock_redo, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_REDO);
-    add_sized (factory, stock_print, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_PRINT);
-    add_sized (factory, stock_search, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_SEARCH);
-    add_sized (factory, stock_search_replace, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_SRCHRPL);
-    add_sized (factory, stock_left_arrow, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_BACK);
-    add_sized (factory, stock_right_arrow, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_FORWARD);
-    add_sized (factory, stock_first, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_FIRST);
-    add_sized (factory, stock_last, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_LAST);
-    add_sized (factory, stock_home, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_HOME);
-    add_sized (factory, stock_stop, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_STOP);
-    add_sized (factory, stock_refresh, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_REFRESH);
-    add_sized (factory, stock_undelete, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_UNDELETE);
+    
     add_sized (factory, stock_timer, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_TIMER);
-    add_sized (factory, stock_timer_stopped, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_TIMER_STOP);
+    
     add_sized (factory, stock_mail, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_MAIL);
     add_sized (factory, stock_mail_receive, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_MAIL_RCV);
     add_sized (factory, stock_mail_send, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_MAIL_SND);
     add_sized (factory, stock_mail_reply, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_MAIL_RPL);
     add_sized (factory, stock_mail_forward, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_MAIL_FWD);
-    add_sized (factory, stock_mail_compose, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_MAIL_NEW);
-    add_sized (factory, stock_trash, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_TRASH);
-    add_sized (factory, stock_trash_full, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_TRASH_FULL);
-    add_sized (factory, stock_spellcheck, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_SPELLCHECK);
-    add_sized (factory, stock_mic, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_MIC);
+    
     add_sized (factory, stock_line_in, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_LINE_IN);
     add_sized (factory, stock_volume, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_VOLUME);
     add_sized (factory, stock_midi, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_MIDI);
-    add_sized (factory, stock_cdrom, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_CDROM);
+    
     add_sized (factory, stock_book_red, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_BOOK_RED);
     add_sized (factory, stock_book_green, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_BOOK_GREEN);
     add_sized (factory, stock_book_blue, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_BOOK_BLUE);
     add_sized (factory, stock_book_yellow, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_BOOK_YELLOW);
-    add_sized (factory, stock_book_open, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_BOOK_OPEN);
-    add_sized (factory, stock_convert, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_CONVERT);
-    add_sized (factory, stock_jump_to, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_JUMP_TO);
+    
     add_sized (factory, stock_menu_about, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_ABOUT);
     /* TODO: I shouldn't waste a pixmap for that */
     add_sized (factory, stock_menu_blank, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_BLANK); 
-    add_sized (factory, stock_up_arrow, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_UP);
-    add_sized (factory, stock_down_arrow, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_DOWN);
-    add_sized (factory, stock_top, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_TOP);
-    add_sized (factory, stock_bottom, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_BOTTOM);
+    
     add_sized (factory, stock_attach, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_ATTACH);
-    add_sized (factory, stock_index, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_INDEX);
-    add_sized (factory, stock_font, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_FONT);
-    add_sized (factory, stock_exec, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_EXEC);
-    add_sized (factory, stock_align_left, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_ALIGN_LEFT);
-    add_sized (factory, stock_align_right, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_ALIGN_RIGHT);
-    add_sized (factory, stock_align_center, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_ALIGN_CENTER);
-    add_sized (factory, stock_align_justify, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_ALIGN_JUSTIFY);
-    add_sized (factory, stock_text_bold, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_TEXT_BOLD);
-    add_sized (factory, stock_text_italic, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_TEXT_ITALIC);
-    add_sized (factory, stock_text_underline, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_TEXT_UNDERLINE);
-    add_sized (factory, stock_text_strikeout, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_TEXT_STRIKEOUT);
-    add_sized (factory, stock_add, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_ADD);
-    add_sized (factory, stock_clear, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_CLEAR);
-    add_sized (factory, stock_colorselector, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_COLORSELECTOR);
-    add_sized (factory, stock_remove, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_REMOVE);
+    
     add_sized (factory, stock_table_borders, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_TABLE_BORDERS);
     add_sized (factory, stock_table_fill, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_TABLE_FILL);
     add_sized (factory, stock_text_bulleted_list, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_TEXT_BULLETED_LIST);
     add_sized (factory, stock_text_numbered_list, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_TEXT_NUMBERED_LIST);
     add_sized (factory, stock_text_indent, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_TEXT_INDENT);
     add_sized (factory, stock_text_unindent, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_PIXMAP_TEXT_UNINDENT);
-    add_sized (factory, stock_button_ok, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_OK);
-    add_sized (factory, stock_button_apply, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_APPLY);
-    add_sized (factory, stock_button_cancel, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_CANCEL);
-    add_sized (factory, stock_button_close, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_CLOSE);
-    add_sized (factory, stock_button_yes, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_YES);
-    add_sized (factory, stock_button_no, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_NO);
-    add_sized (factory, stock_help, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_HELP);
-    add_sized (factory, stock_right_arrow, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_NEXT);
-    add_sized (factory, stock_left_arrow, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_PREV);
-    add_sized (factory, stock_up_arrow, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_UP);
-    add_sized (factory, stock_down_arrow, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_DOWN);
-    add_sized (factory, stock_font, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_BUTTON_FONT);
-    add_sized (factory, stock_new, GTK_ICON_SIZE_MENU, GNOME_STOCK_MENU_NEW);
+    
     add_sized (factory, stock_menu_scores, GNOME_ICON_SIZE_TOOLBAR, GNOME_STOCK_MENU_SCORES);
 }
 
 static GtkStockItem builtin_items [] =
 {
-    { GNOME_STOCK_PIXMAP_ADD, N_("Add"), 0, 0, GETTEXT_PACKAGE },
-    { GNOME_STOCK_PIXMAP_CLEAR, N_("Clear"), 0, 0, GETTEXT_PACKAGE },
-    { GNOME_STOCK_PIXMAP_COLORSELECTOR, N_("Select Color"), 0, 0, GETTEXT_PACKAGE },
-    { GNOME_STOCK_PIXMAP_REMOVE, N_("Remove"), 0, 0, GETTEXT_PACKAGE },
     { GNOME_STOCK_PIXMAP_TABLE_BORDERS, N_("Table Borders"), 0, 0, GETTEXT_PACKAGE },
     { GNOME_STOCK_PIXMAP_TABLE_FILL, N_("Table Fill"), 0, 0, GETTEXT_PACKAGE },
     { GNOME_STOCK_PIXMAP_TEXT_BULLETED_LIST, N_("Bulleted List"), 0, 0, GETTEXT_PACKAGE },
--- libgnomeui/gnome-stock-icons.h
+++ libgnomeui/gnome-stock-icons.h
@@ -36,32 +36,12 @@ GtkIconSize gnome_icon_size_toolbar;
 
 extern void init_gnome_stock_icons (void);
 
-#define GNOME_STOCK_PIXMAP_NEW "gnome-stock-pixmap-new"
-#define GNOME_STOCK_PIXMAP_OPEN "gnome-stock-pixmap-open"
-#define GNOME_STOCK_PIXMAP_CLOSE "gnome-stock-pixmap-close"
-#define GNOME_STOCK_PIXMAP_REVERT "gnome-stock-pixmap-revert"
-#define GNOME_STOCK_PIXMAP_SAVE "gnome-stock-pixmap-save"
-#define GNOME_STOCK_PIXMAP_SAVE_AS "gnome-stock-pixmap-save-as"
-#define GNOME_STOCK_PIXMAP_CUT "gnome-stock-pixmap-cut"
-#define GNOME_STOCK_PIXMAP_COPY "gnome-stock-pixmap-copy"
-#define GNOME_STOCK_PIXMAP_PASTE "gnome-stock-pixmap-paste"
-#define GNOME_STOCK_PIXMAP_CLEAR "gnome-stock-pixmap-clear"
-#define GNOME_STOCK_PIXMAP_PROPERTIES "gnome-stock-pixmap-properties"
-#define GNOME_STOCK_PIXMAP_PREFERENCES "gnome-stock-pixmap-preferences"
-#define GNOME_STOCK_PIXMAP_HELP "gnome-stock-pixmap-help"
 #define GNOME_STOCK_PIXMAP_SCORES "gnome-stock-pixmap-scores"
-#define GNOME_STOCK_PIXMAP_PRINT "gnome-stock-pixmap-print"
-#define GNOME_STOCK_PIXMAP_SEARCH "gnome-stock-pixmap-search"
+
 #define GNOME_STOCK_PIXMAP_SRCHRPL "gnome-stock-pixmap-srchrpl"
 #define GNOME_STOCK_PIXMAP_BACK "gnome-stock-pixmap-back"
 #define GNOME_STOCK_PIXMAP_FORWARD "gnome-stock-pixmap-forward"
-#define GNOME_STOCK_PIXMAP_FIRST "gnome-stock-pixmap-first"
-#define GNOME_STOCK_PIXMAP_LAST "gnome-stock-pixmap-last"
-#define GNOME_STOCK_PIXMAP_HOME "gnome-stock-pixmap-home"
-#define GNOME_STOCK_PIXMAP_STOP "gnome-stock-pixmap-stop"
-#define GNOME_STOCK_PIXMAP_REFRESH "gnome-stock-pixmap-refresh"
-#define GNOME_STOCK_PIXMAP_UNDO "gnome-stock-pixmap-undo"
-#define GNOME_STOCK_PIXMAP_REDO "gnome-stock-pixmap-redo"
+
 #define GNOME_STOCK_PIXMAP_TIMER "gnome-stock-pixmap-timer"
 #define GNOME_STOCK_PIXMAP_TIMER_STOP "gnome-stock-pixmap-timer-stop"
 #define GNOME_STOCK_PIXMAP_MAIL "gnome-stock-pixmap-mail"
@@ -70,13 +50,12 @@ extern void init_gnome_stock_icons (void);
 #define GNOME_STOCK_PIXMAP_MAIL_RPL "gnome-stock-pixmap-mail-rpl"
 #define GNOME_STOCK_PIXMAP_MAIL_FWD "gnome-stock-pixmap-mail-fwd"
 #define GNOME_STOCK_PIXMAP_MAIL_NEW "gnome-stock-pixmap-mail-new"
-#define GNOME_STOCK_PIXMAP_TRASH "gnome-stock-pixmap-trash"
+
 #define GNOME_STOCK_PIXMAP_TRASH_FULL "gnome-stock-pixmap-trash-full"
-#define GNOME_STOCK_PIXMAP_UNDELETE "gnome-stock-pixmap-undelete"
-#define GNOME_STOCK_PIXMAP_SPELLCHECK "gnome-stock-pixmap-spellcheck"
+
 #define GNOME_STOCK_PIXMAP_MIC "gnome-stock-pixmap-mic"
 #define GNOME_STOCK_PIXMAP_LINE_IN "gnome-stock-pixmap-line-in"
-#define GNOME_STOCK_PIXMAP_CDROM "gnome-stock-pixmap-cdrom"
+
 #define GNOME_STOCK_PIXMAP_VOLUME "gnome-stock-pixmap-volume"
 #define GNOME_STOCK_PIXMAP_MIDI "gnome-stock-pixmap-midi"
 #define GNOME_STOCK_PIXMAP_BOOK_RED "gnome-stock-pixmap-book-red"
@@ -88,16 +67,12 @@ extern void init_gnome_stock_icons (void);
 #define GNOME_STOCK_PIXMAP_QUIT "gnome-stock-pixmap-quit"
 #define GNOME_STOCK_PIXMAP_MULTIPLE "gnome-stock-pixmap-multiple"
 #define GNOME_STOCK_PIXMAP_NOT "gnome-stock-pixmap-not"
-#define GNOME_STOCK_PIXMAP_CONVERT "gnome-stock-pixmap-convert"
+
 #define GNOME_STOCK_PIXMAP_JUMP_TO "gnome-stock-pixmap-jump-to"
 #define GNOME_STOCK_PIXMAP_UP "gnome-stock-pixmap-up"
 #define GNOME_STOCK_PIXMAP_DOWN "gnome-stock-pixmap-down"
-#define GNOME_STOCK_PIXMAP_TOP "gnome-stock-pixmap-top"
-#define GNOME_STOCK_PIXMAP_BOTTOM "gnome-stock-pixmap-bottom"
+
 #define GNOME_STOCK_PIXMAP_ATTACH "gnome-stock-pixmap-attach"
-#define GNOME_STOCK_PIXMAP_INDEX "gnome-stock-pixmap-index"
-#define GNOME_STOCK_PIXMAP_FONT "gnome-stock-pixmap-font"
-#define GNOME_STOCK_PIXMAP_EXEC "gnome-stock-pixmap-exec"
 
 #define GNOME_STOCK_PIXMAP_ALIGN_LEFT "gnome-stock-pixmap-align-left"
 #define GNOME_STOCK_PIXMAP_ALIGN_RIGHT "gnome-stock-pixmap-align-right"
@@ -110,10 +85,6 @@ extern void init_gnome_stock_icons (void);
 #define GNOME_STOCK_PIXMAP_TEXT_STRIKEOUT "gnome-stock-pixmap-text-strikeout"
 #define GNOME_STOCK_PIXMAP_TEXT_INDENT "gnome-stock-pixmap-text-indent"
 #define GNOME_STOCK_PIXMAP_TEXT_UNINDENT "gnome-stock-pixmap-text-unindent"
-#define GNOME_STOCK_PIXMAP_COLORSELECTOR "gnome-stock-pixmap-colorselector"
-
-#define GNOME_STOCK_PIXMAP_ADD "gnome-stock-pixmap-add"
-#define GNOME_STOCK_PIXMAP_REMOVE "gnome-stock-pixmap-remove"
 
 #define GNOME_STOCK_PIXMAP_TABLE_BORDERS "gnome-stock-pixmap-table-borders"
 #define GNOME_STOCK_PIXMAP_TABLE_FILL "gnome-stock-pixmap-table-fill"
@@ -128,12 +99,12 @@ extern void init_gnome_stock_icons (void);
  * Buttons
  */
 
-#define GNOME_STOCK_BUTTON_OK "gnome-stock-button-ok"
-#define GNOME_STOCK_BUTTON_CANCEL "gnome-stock-button-cancel"
-#define GNOME_STOCK_BUTTON_YES "gnome-stock-button-yes"
-#define GNOME_STOCK_BUTTON_NO "gnome-stock-button-no"
-#define GNOME_STOCK_BUTTON_CLOSE "gnome-stock-button-close"
-#define GNOME_STOCK_BUTTON_APPLY "gnome-stock-button-apply"
+#define GNOME_STOCK_BUTTON_OK "gnome-"
+#define GNOME_STOCK_BUTTON_CANCEL "gnome-"
+#define GNOME_STOCK_BUTTON_YES "gnome-"
+#define GNOME_STOCK_BUTTON_NO "gnome-"
+#define GNOME_STOCK_BUTTON_CLOSE "gnome-"
+#define GNOME_STOCK_BUTTON_APPLY "gnome-"
 #define GNOME_STOCK_BUTTON_HELP "gnome-stock-button-help"
 #define GNOME_STOCK_BUTTON_NEXT "gnome-stock-button-next"
 #define GNOME_STOCK_BUTTON_PREV "gnome-stock-button-prev"
@@ -146,47 +117,31 @@ extern void init_gnome_stock_icons (void);
  */
 
 #define GNOME_STOCK_MENU_BLANK "gnome-stock-menu-blank"
-#define GNOME_STOCK_MENU_NEW "gnome-stock-menu-new"
-#define GNOME_STOCK_MENU_SAVE "gnome-stock-menu-save"
-#define GNOME_STOCK_MENU_SAVE_AS "gnome-stock-menu-save-as"
-#define GNOME_STOCK_MENU_REVERT "gnome-stock-menu-revert"
-#define GNOME_STOCK_MENU_OPEN "gnome-stock-menu-open"
-#define GNOME_STOCK_MENU_CLOSE "gnome-stock-menu-close"
-#define GNOME_STOCK_MENU_QUIT "gnome-stock-menu-quit"
-#define GNOME_STOCK_MENU_CUT "gnome-stock-menu-cut"
-#define GNOME_STOCK_MENU_COPY "gnome-stock-menu-copy"
-#define GNOME_STOCK_MENU_PASTE "gnome-stock-menu-paste"
+
 #define GNOME_STOCK_MENU_PROP "gnome-stock-menu-prop"
 #define GNOME_STOCK_MENU_PREF "gnome-stock-menu-pref"
 #define GNOME_STOCK_MENU_ABOUT "gnome-stock-menu-about"
 #define GNOME_STOCK_MENU_SCORES "gnome-stock-menu-scores"
-#define GNOME_STOCK_MENU_UNDO "gnome-stock-menu-undo"
-#define GNOME_STOCK_MENU_REDO "gnome-stock-menu-redo"
-#define GNOME_STOCK_MENU_PRINT "gnome-stock-menu-print"
-#define GNOME_STOCK_MENU_SEARCH "gnome-stock-menu-search"
+
 #define GNOME_STOCK_MENU_SRCHRPL "gnome-stock-menu-srchrpl"
 #define GNOME_STOCK_MENU_BACK "gnome-stock-menu-back"
 #define GNOME_STOCK_MENU_FORWARD "gnome-stock-menu-forward"
-#define GNOME_STOCK_MENU_FIRST "gnome-stock-menu-first"
-#define GNOME_STOCK_MENU_LAST "gnome-stock-menu-last"
-#define GNOME_STOCK_MENU_HOME "gnome-stock-menu-home"
-#define GNOME_STOCK_MENU_STOP "gnome-stock-menu-stop"
-#define GNOME_STOCK_MENU_REFRESH "gnome-stock-menu-refresh"
+
 #define GNOME_STOCK_MENU_MAIL "gnome-stock-menu-mail"
 #define GNOME_STOCK_MENU_MAIL_RCV "gnome-stock-menu-mail-rcv"
 #define GNOME_STOCK_MENU_MAIL_SND "gnome-stock-menu-mail-snd"
 #define GNOME_STOCK_MENU_MAIL_RPL "gnome-stock-menu-mail-rpl"
 #define GNOME_STOCK_MENU_MAIL_FWD "gnome-stock-menu-mail-fwd"
 #define GNOME_STOCK_MENU_MAIL_NEW "gnome-stock-menu-mail-new"
-#define GNOME_STOCK_MENU_TRASH "gnome-stock-menu-trash"
+
 #define GNOME_STOCK_MENU_TRASH_FULL "gnome-stock-menu-trash-full"
-#define GNOME_STOCK_MENU_UNDELETE "gnome-stock-menu-undelete"
+
 #define GNOME_STOCK_MENU_TIMER "gnome-stock-menu-timer"
 #define GNOME_STOCK_MENU_TIMER_STOP "gnome-stock-menu-timer-stop"
-#define GNOME_STOCK_MENU_SPELLCHECK "gnome-stock-menu-spellcheck"
+
 #define GNOME_STOCK_MENU_MIC "gnome-stock-menu-mic"
 #define GNOME_STOCK_MENU_LINE_IN "gnome-stock-menu-line-in"
-#define GNOME_STOCK_MENU_CDROM "gnome-stock-menu-cdrom"
+
 #define GNOME_STOCK_MENU_VOLUME "gnome-stock-menu-volume"
 #define GNOME_STOCK_MENU_MIDI "gnome-stock-menu-midi"
 #define GNOME_STOCK_MENU_BOOK_RED "gnome-stock-menu-book-red"
@@ -194,16 +149,12 @@ extern void init_gnome_stock_icons (void);
 #define GNOME_STOCK_MENU_BOOK_BLUE "gnome-stock-menu-book-blue"
 #define GNOME_STOCK_MENU_BOOK_YELLOW "gnome-stock-menu-book-yellow"
 #define GNOME_STOCK_MENU_BOOK_OPEN "gnome-stock-menu-book-open"
-#define GNOME_STOCK_MENU_CONVERT "gnome-stock-menu-convert"
+
 #define GNOME_STOCK_MENU_JUMP_TO "gnome-stock-menu-jump-to"
 #define GNOME_STOCK_MENU_UP "gnome-stock-menu-up"
 #define GNOME_STOCK_MENU_DOWN "gnome-stock-menu-down"
-#define GNOME_STOCK_MENU_TOP "gnome-stock-menu-top"
-#define GNOME_STOCK_MENU_BOTTOM "gnome-stock-menu-bottom"
+
 #define GNOME_STOCK_MENU_ATTACH "gnome-stock-menu-attach"
-#define GNOME_STOCK_MENU_INDEX "gnome-stock-menu-index"
-#define GNOME_STOCK_MENU_FONT "gnome-stock-menu-font"
-#define GNOME_STOCK_MENU_EXEC "gnome-stock-menu-exec"
 
 #define GNOME_STOCK_MENU_ALIGN_LEFT "gnome-stock-menu-align-left"
 #define GNOME_STOCK_MENU_ALIGN_RIGHT "gnome-stock-menu-align-right"
