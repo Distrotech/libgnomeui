67399e289b0681a15c0ce3ba60f7bc5054548a10
Mark McLoughlin <markmc@sun.com>
2003-10-09 11:13:01 +0000

    re-license to LGPL. It should never have been GPL ...
    2003-10-08  Mark McLoughlin  <markmc@sun.com>
    
            * libgnomeui/libgnomeui-access.[ch]: re-license to LGPL.
            It should never have been GPL ...

--- libgnomeui/libgnomeui-access.h
+++ libgnomeui/libgnomeui-access.h
@@ -4,31 +4,24 @@
  *
  * Lib GNOME UI Accessibility support module
  *
- * This code copied from Wipro's panel-access.c.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
- *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #ifndef __LIBGNOMEUI_ACCESS_H__
 #define __LIBGNOMEUI_ACCESS_H__
-/*
- * Accessibility convenience functions.
- *
- * Copyright 2002, Sun Microsystems.
- */
 
 #include <glib-object.h>
 #include <atk/atkobject.h>
