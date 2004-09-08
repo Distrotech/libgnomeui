18ac18ef937e9906a90110f0a9f9693f2b240694
Anders Carlsson <andersca@gnome.org>
2004-09-08 07:28:41 +0000

    s/uint/guint/
    2004-09-08  Anders Carlsson  <andersca@gnome.org>
    
    	* libgnomeui/gnome-icon-list.c: s/uint/guint/

--- libgnomeui/gnome-icon-list.c
+++ libgnomeui/gnome-icon-list.c
@@ -572,7 +572,7 @@ gil_unselect_all (GnomeIconList *gil, GdkEvent *event, gpointer keep)
 void
 gnome_icon_list_select_all (GnomeIconList *gil)
 {
-	uint i;
+	guint i;
 	
 	g_return_if_fail (gil != NULL);
 	g_return_if_fail (IS_GIL (gil));
