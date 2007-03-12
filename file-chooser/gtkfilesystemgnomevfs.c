cbec73992a55134f95ebfcc44fc138003060768c
Kjartan Maraas <kmaraas@src.gnome.org>
2007-03-12 12:12:00 +0000

    Revert accidental change
    svn path=/trunk/; revision=5305

--- file-chooser/gtkfilesystemgnomevfs.c
+++ file-chooser/gtkfilesystemgnomevfs.c
@@ -2852,7 +2852,6 @@ gtk_file_folder_gnome_vfs_dispose (GObject *object)
       g_object_ref (object);
       g_idle_add ((GSourceFunc)unref_at_idle, object);
     }
-  folder_parent_class->dispose (object);
 }
 
 
